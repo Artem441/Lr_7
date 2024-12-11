7e5f071bb9262a2f09a6757329df8903494d81cc
Artem Belchikov <artpres444@gmail.com>
2024-12-11 20:06:22 +0300

    second commit

---
+++ out/build/task_0/CMakeFiles/Task_0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_0.dir/Task_0.cpp.o"
+  "CMakeFiles/Task_0.dir/Task_0.cpp.o.d"
+  "Task_0"
+  "Task_0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
